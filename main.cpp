f58f633d94204cbe6f86bfd6a74078611c8756ea
jaycode8 <jamesmumo443@gmail.com>
2026-02-11 19:49:00 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,38 @@
+#include <iomanip>
+#include <ios>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char *argv[]) { return 0; }
+int WIDTH = 15;
+
+class Student {
+  std::string fullName;
+  int admNo;
+  int form;
+
+public:
+  Student(const std::string &n, const int &a, const int &f)
+      : fullName(n), admNo(a), form(f) {}
+
+  std::string getName() { return fullName; }
+
+  int getAdmission() { return admNo; }
+
+  int getForm() { return form; }
+};
+
+int main(int argc, char *argv[]) {
+  std::vector<Student> students = {{"Jane Doe", 1456, 2},
+                                   {"Mutua Munywoki", 5467, 3}};
+
+  std::cout << std::left;
+  std::cout << std::setw(WIDTH) << "Name" << std::setw(WIDTH) << "AdmNo."
+            << std::setw(WIDTH) << "Form" << std::endl;
+
+  for (auto &s : students) {
+    std::cout << std::setw(WIDTH) << s.getName() << std::setw(WIDTH)
+              << s.getAdmission() << std::setw(WIDTH) << s.getForm()
+              << std::endl;
+  }
+  return 0;
+}
